18c55ac15805282234744c95f6f72790d247c6ed
chep-project-amazonClone <manish356111@gmail.com>
2024-11-15 19:34:27 +0530

    make the cart qty interative

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,6 +77,10 @@ document.querySelectorAll('.js-add-to-cart') //select the ATC button and put val
       quantity: 1
      });
     }
-   console.log(cart);
+    let cartQuantity = 0;
+    cart.forEach((item)=>{
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
   });
 });
