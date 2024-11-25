2fc87749f49d7fc08025898750c1f63754d6ecac
chep-project-amazonClone <manish356111@gmail.com>
2024-11-25 15:24:08 +0530

    testing

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,3 +1,6 @@
 import {renderOrderSummary} from '../scripts/checkout/orderSummary.js';
+import { renderPaymentSummary } from './checkout/paymentSummary.js';
 
+
+renderPaymentSummary();
 renderOrderSummary();
