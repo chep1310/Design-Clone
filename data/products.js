7256cd7534843484f1ee3d24e8239dca61c260a1
chep-project-amazonClone <manish356111@gmail.com>
2024-12-09 02:26:50 +0530

    added size chart using inhertiacne

--- data/products.js
+++ data/products.js
@@ -25,15 +25,55 @@ class Product{
     this.name = productDetails.name;
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
-  }
-
+  };
   getStarsUrl(){
    return `images/ratings/rating-${this.rating.stars*10}.png`
   };
   getPrice(){
   return `$${formatCurrency(this.priceCents)}`
   };
+  extraInfoHTML(){
+    return '';
+  }
 }
+
+
+//inheritance = child class clothing
+class Clothing extends Product{
+
+ sizeChartLink;
+ constructor(productDetails){
+  super(productDetails);
+  this.sizeChartLink = productDetails.sizeChartLink;
+ }
+
+ extraInfoHTML(){
+  return `
+  <a href="${this.sizeChartLink}" target="_blank">Size Chart</a>
+  `;
+ }
+};
+
+const tshirt = new Clothing({
+  id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+  image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+  name: "Adults Plain Cotton T-Shirt - 2 Pack",
+  rating: {
+    stars: 4.5,
+    count: 56
+  },
+  priceCents: 799,
+  keywords: [
+    "tshirts",
+    "apparel",
+    "mens"
+  ],
+  type: "clothing", //discrimating property
+  sizeChartLink: "images/clothing-size-chart.png"
+});
+
+
+
 
 export const products = [
   {
@@ -695,5 +735,8 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type === 'clothing'){
+    return new Clothing(productDetails);
+  }
   return new Product(productDetails);
 });
